e7151b25b73a7b685cd2487b1bd8fb93689ffadf
travistaylor717 <travistaylor717@gmail.com>
2026-03-14 13:09:18 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
   },
   images: { unoptimized: true },
   output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/supabase/functions/**', '**/node_modules/**'],
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
